a3c81d98fdc54931f178c1bda4034fc00862165e
jedwible <69050289+jedwible@users.noreply.github.com>
2022-08-29 16:26:14 -0400

    update

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+console.log(Boolean(1));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
+const money = 0;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
