25de15d4e8e6c30e23721fb2bf583a04b8453f88
jedwible <69050289+jedwible@users.noreply.github.com>
2023-03-02 16:15:16 -0500

    updates

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,7 +12,6 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +26,7 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = 'Correct Answer!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     //score still greater than 0
   } else if (score > 1) {
